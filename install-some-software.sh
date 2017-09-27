68ed3c91efc424ee7016628cbf5912fbe6cb6706
Lim, Thing-Han <b02901144@ntu.edu.tw>
2017-09-27 17:56:47 +0800

    Install some software

---
+++ install-some-software.sh
@@ -0,0 +1,31 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: install-some-software.sh
+# 
+#         USAGE: ./install-some-software.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Lim Thing-Han, 
+#  ORGANIZATION: 
+#       CREATED: 09/27/2017 11:42
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+### make sure /etc/locale.conf has been written correctly
+sudo pacman -S ntfs-3g
+sudo pacman -S gnome-terminal		
+
+### gnome-screenshot
+sudo pacman -S screenshot
+gsettings set org.gnome.gnome-screenshot auto-save-directory "file:///home/$USER/Pictures/ScreenShots"
+
+sudo pacman -S mplayer okular
+
+
